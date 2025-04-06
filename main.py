0d4b3ce70c07dde2bcde7ffd39d542dc4a503215
Emilio Garzia <emilio.garzia001@studenti.uniparthenope.it>
2025-04-07 01:08:21 +0200

    ì

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from tic_tac_toe import TicTacToe
-from argparse import ArgumentParser
 
 if __name__ == "__main__":
     game = TicTacToe()
     
     # Start CPU vs CPU game simulation
-    #game.cpu_vs_cpu_AI_simulation(print_moves=True, algorithm=game.MINIMAX, smart_level=game.MEDIUM)
+    game.cpu_vs_cpu_AI_simulation(print_moves=True, algorithm=game.MINIMAX, smart_level=game.MEDIUM)
 
     # Print mapping of the board
     game.print_mapping()
 
     # Start Player vs CPU game
-    game.player_vs_cpu(smart_level=game.MEDIUM, player1=True)
+    #game.player_vs_cpu(smart_level=game.MEDIUM, player1=True)
 
     # Player vs Player game
     """
